b7eaa486fa668873dd7fb3af1da25b7579ce8e71
SabrinaHersh <hershs4@uw.edu>
2017-10-26 14:52:22 -0700

    completed

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -5,6 +5,7 @@ library(httr)
 
 # Read in your api-key.R file (that you'll create). Store your api-key in a variable in that file.
 # Write a function that allows you to specify a movie, that does the following:
+source("api-key.R")
 GetReview <- function(movie) {
   
   # Construct a search query using YOUR api key
@@ -12,22 +13,29 @@ GetReview <- function(movie) {
   # Your parameters should include a "query" and an "api_key"
   # Note: the HTTR library will take care of spaces in the arguments
   # See the interactive console for more detail:https://developer.nytimes.com/movie_reviews_v2.json#/Console/GET/reviews/search.json
-  
+  query.params <- list(query = movie, api_key = api.key)
+  response <- GET("https://api.nytimes.com/svc/movies/v2/reviews/search.json", query = query.params)
+  body <- content(response, "text")
   
   # Request data using your search query
-  
+  results <- fromJSON(body)
   
   # What type of variable does this return?
+  #list
   
   # Flatten the data stored in the `$results` key of the data returned to you
-  
+  flattened <- flatten(results$results)
   
   # From the most recent review, store the headline, short summary, and link to full article each in their own variables
-  
+  first.review <- flattened[1, ]
+  headline <- first.review$headline
+  summary <- first.review$summary_short
+  link <- first.review$link.url
 
   # Return an list of the three pieces of information from above
-  
+  info <- list(headline = headline, summary = summary, link = link)
+  return(info)
 }
 
 # Test that your function works with a movie of your choice
-
+high.fidelity.info <- GetReview("High Fidelity")
